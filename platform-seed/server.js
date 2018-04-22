be6835bfad10c56cc14371134f42c5d1df4a92e3
DJ <dj.bowden7@gmail.com>
2018-04-22 12:14:28 -0400

    updated server.js to use middleware

--- platform-seed/server.js
+++ platform-seed/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const domainAdapter = require('./utils/domain-adapter');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//use middleware for determining request location and set country code.
+app.use(domainAdapter);
+
+app.get('/', (req, res) => res.send(`Hello World! ${req.countryCode}`));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
