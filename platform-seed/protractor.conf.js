4d36c7e479b88890c1c3552985d5d7571aea4749
DJ <dj.bowden7@gmail.com>
2018-04-22 09:33:13 -0400

    dependencies and configurations for cucumber bdd

--- platform-seed/protractor.conf.js
+++ platform-seed/protractor.conf.js
@@ -5,24 +5,36 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
+  //set spec to be features
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    './e2e/features/**/*.feature'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
+  //custom for enabling cucumber protractor
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {
+    require: ['./e2e/**/*.ts'],
+    tags: [],
+    strict: true,
+    format: ["progress"],
+    dryRun: false,
+    compiler: [ 'ts:ts-node']
+  },
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
-  onPrepare() {
+  onPrepare() { 
+    browser.manage().window().maximize(); // maximize the browser before executing the feature files
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    // jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
