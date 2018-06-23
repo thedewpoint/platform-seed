aa30e63a7f215eeb10758f0dd76d5a9e904df703
DJ <dj.bowden7@gmail.com>
2018-06-23 18:55:44 -0400

    feat(angular module): added weather module import via npm link

--- platform-seed/src/app/test-div/test-div.component.ts
+++ platform-seed/src/app/test-div/test-div.component.ts
@@ -2,23 +2,22 @@ import { Component, OnInit, Inject } from '@angular/core';
 import {Toggle} from '../gatekeeper/toggle.decorator';
 import { Config } from '../config/config.decorator';
 import {WeatherService} from '../weather/weather.service';
+import {WeatherTestModel} from 'weather';
 @Component({
   selector: 'test-div',
   templateUrl: './test-div.component.html',
   styleUrls: ['./test-div.component.css']
 })
 export class TestDivComponent implements OnInit {
-  
   @Toggle('${countryCode}.showHumidity')
   showHumidity;
- 
  @Config()
   locationCode;
 
   weather;
-  
   constructor(private _weatherService: WeatherService) {}
   ngOnInit() {
+    let test: WeatherTestModel; 
     this._weatherService.getWeather(this.locationCode).subscribe((weather)=>{
       this.weather = weather;
     });
